92f4ed0eb10507e3e563aad088d90caa9dcc61aa
pjohans <pjohans@gmail.com>
2012-04-22 22:48:24 +0200

    another solution

--- templates/page--user--reset.tpl.php
+++ templates/page--user--reset.tpl.php
@@ -1,10 +1,5 @@
 <?php
-$params = $_SERVER['QUERY_STRING'];
-
-$path = base_path();
 $params = $_SERVER['REQUEST_URI'];
-$args = str_replace($path,'',$params);
-
-drupal_goto('user/password_reset/'.$args);
+drupal_goto('user/password_reset'.$params);
 ?>
 
